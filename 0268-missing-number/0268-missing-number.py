80ff6d0dd60e685730ae1c7c1204c58a7f81ab39
Konthala Dinesh <konthaladinesh0152@gmail.com>
2026-09-23 19:07:59 +0530

    Time: 3 ms (58.2%), Space: 14 MB (7.25%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        result = set(nums)
+        for i in range(len(result)+1):
+            if i not in result:
+                return i
+            result.add(i)
+
+        
